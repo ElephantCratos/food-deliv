88ee24e8b4d0441f507c9810f73b2fb77bf4c760
ADLAI2040 <rozmahov.ilia99@mail.ru>
2024-04-26 10:51:10 +0500

    Quantity show

--- resources/views/All_Orders.blade.php
+++ resources/views/All_Orders.blade.php
@@ -45,36 +45,37 @@
 
         @foreach ($Order as $order)
 
-                <div class="flex justify-center p-4">
+        <div class="flex justify-center p-4">
 
-                    <div class="order-block bg-white border border-gray-200 rounded-lg p-4 shadow-md" style="width: 70%;">
-                        <h3 class="text-lg font-semibold text-gray-800">Order {{$order->id}}</h3>
-                        <p class="text-gray-600">Status: {{$order->status->name}}</p>
+            <div class="order-block bg-white border border-gray-200 rounded-lg p-4 shadow-md" style="width: 70%;">
+                <h3 class="text-lg font-semibold text-gray-800">Order {{$order->id}}</h3>
+                <p class="text-gray-600">Status: {{$order->status->name}}</p>
 
-                        <ul class="list-disc list-inside text-gray-600">
-                            @foreach($order->positions as $position)
-                            <li>{{$position->dish->name}} - ${{$position->price}} - @foreach ($position->ingredients as $pos) {{$pos->name}} @endforeach</li>
-                            @endforeach
+                <ul class="list-disc list-inside text-gray-600">
+                    @foreach($order->positions as $position)
+                    <li>{{$position->dish->name}} - ${{$position->price}} - @foreach ($position->ingredients as $pos) {{$pos->name}} @endforeach</li>
+                    <li>Количество: {{ $position->quantity }}</li>
+                    @endforeach
 
-                        </ul>
-                        <p class="text-gray-800 font-semibold">Total Price: {{ $order->price }}</p>
-                        <div class="flex justify-between mt-4">
+                </ul>
+                <p class="text-gray-800 font-semibold">Total Price: {{ $order->price }}</p>
+                <div class="flex justify-between mt-4">
 
-                            @if($order->status_id == 1)
-                                <form action="{{ route('orders.accept', ['id' => $order->id]) }}" method="POST">
-                                    @csrf
-                                    @method('POST')
-                                    <button type="submit" class="bg-green-500 hover:bg-green-700 text-black font-bold py-2 px-4 rounded">Accept</button>
-                                </form>
-                                <form action="{{ route('orders.decline', ['id' => $order->id]) }}" method="POST">
-                                    @csrf
-                                    @method('POST')
-                                    <button type="submit" class="bg-green-500 hover:bg-green-700 text-black font-bold py-2 px-4 rounded">Decline</button>
-                                </form>
-                            @endif
-                        </div>
-                    </div>
+                    @if($order->status_id == 1)
+                    <form action="{{ route('orders.accept', ['id' => $order->id]) }}" method="POST">
+                        @csrf
+                        @method('POST')
+                        <button type="submit" class="bg-green-500 hover:bg-green-700 text-black font-bold py-2 px-4 rounded">Accept</button>
+                    </form>
+                    <form action="{{ route('orders.decline', ['id' => $order->id]) }}" method="POST">
+                        @csrf
+                        @method('POST')
+                        <button type="submit" class="bg-green-500 hover:bg-green-700 text-black font-bold py-2 px-4 rounded">Decline</button>
+                    </form>
+                    @endif
                 </div>
+            </div>
+        </div>
         @endforeach
     </section>
 </x-app-layout>
